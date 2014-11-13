98a4aeaba0ccced82cc7c3ff86df1f5df21eb879
Dolgovec <Dolgovec@rambler.ru>
2014-11-13 18:13:15 +0200

    v 2.2.1
    еще больше гавнокода!

--- rating.js
+++ rating.js
@@ -1,5 +1,6 @@
 /**
  * Created by boshurik on 13.11.14.
+ * Ara)))
  */
 $(document).ready(function(){
     $('.down_rating').click(function(){
@@ -9,7 +10,10 @@ $(document).ready(function(){
         $(this).parent().find('.how_much_left_popup').addClass('minus');
         $(this).parent().find('.today_rating').hide();
         $(this).parent().find('.streamer_rating').show();
+		$(this).parent().find('.article_rating').show();
+		$(this).parent().find('.article_comment_rating').show();
         $(this).parent().find('.plus').hide();
+		$(this).parent().find('.total_comment_rating').hide();
         $(this).parent().find('.minus').show();
     });
     $('.up_rating').click(function(){
@@ -19,7 +23,10 @@ $(document).ready(function(){
         $(this).parent().find('.how_much_left_popup').addClass('plus');
         $(this).parent().find('.today_rating').hide();
         $(this).parent().find('.streamer_rating').show();
+		$(this).parent().find('.article_rating').show();
+		$(this).parent().find('.article_comment_rating').show();
         $(this).parent().find('.minus').hide();
+		$(this).parent().find('.total_comment_rating').hide();
         $(this).parent().find('.plus').show();
 
     });
